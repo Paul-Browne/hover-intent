b88e68e8d612f1c75a4bbe4540dd03ee1a4f3c99
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-10-08 13:47:26 +0300

    Update hover-intent.js

--- hover-intent.js
+++ hover-intent.js
@@ -1,36 +1,58 @@
 /*! hover-intent.js | MIT license | Paul Browne */ 
 (function() {
-  var h = document.getElementsByTagName("html")[0];
-  if (!(('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0))) {
-    h.className += " no-touch";
-  } else {
-    h.className += " touch";
+
+  //  We are interested in list items 
+  //  that are nested within the nav
+
+  var li = document.getElementsByTagName("li"),
+      nav = document.getElementsByTagName("nav")[0];
+
+  //  function will check all the list items
+  //  and see if each one is nested within the nav
+
+  function childOf(a, b){
+    while((a=a.parentNode)&&a!==b);
+    return !!a;
   }
 
-  function z(p, tt) {
+  //  function that returns when the mouse 
+  //  stops moving for xxx milliseconds.
+  //  A good range is 150-350, less than 
+  //  150 it's too quick, greater than 350
+  //  and it's too slow. IMO
+
+  function stillMouse(a, b) {
     window.addEventListener("mousemove", function() {
-      clearTimeout(tt);
-      tt = setTimeout(p, 550);
+      clearTimeout(b);
+      b = setTimeout(a, 250);
     });
-    return p;
+    return a;
   }
 
-  function b() {
+  function hoverIntent() {
     
-    c[d].addEventListener("mouseenter", function() {
+    //  attach the listeners to all the list items
+    
+    li[i].addEventListener("mouseenter", function() {
+      
+      //  reference "this" for use later within the stillMouse function
+      
       var b = this;
+      
+      //  add the class 'hi-hover' when mouse enters the list item
+      
       this.className += " hi-hover";
-        z(function() {
+        stillMouse(function() {
           if(b.className.match(/(?:^|\s)hi-hover(?!\S)/) && !b.className.match(/(?:^|\s)hi-intent(?!\S)/)){
             b.className += " hi-intent";
           }
         });
     });
 
-    c[d].addEventListener("mouseleave", function() {
+    li[i].addEventListener("mouseleave", function() {
       var b = this;
       this.className = this.className.replace(/(?:^|\s)hi-hover(?!\S)/g, "");
-        z(function() {
+        stillMouse(function() {
           if(b.className.match(/(?:^|\s)hi-intent(?!\S)/) && !b.className.match(/(?:^|\s)hi-hover(?!\S)/)){
             b.className = b.className.replace(/(?:^|\s)hi-intent(?!\S)/g, "");
           }
@@ -38,6 +60,11 @@
     });
     
   }
-  for (var c = document.getElementsByTagName("li"), d = 0; d < c.length; ++d) b();
   
+  for (var i = 0; i < li.length; ++i) {
+    var navli = childOf(li[i], nav);
+    if(navli){
+      hoverIntent();
+    }
+  }
 })();
