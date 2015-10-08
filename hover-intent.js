21f66016c6c5647a2fc8dd402c65aaff5b1eafed
paulbrowne <Paulmatthewbrowne@gmail.com>
2015-10-08 14:11:01 +0300

    Update hover-intent.js

--- hover-intent.js
+++ hover-intent.js
@@ -15,7 +15,7 @@
     return !!a;
   }
 
-  //  function that returns when the mouse 
+  //  function that fires when the mouse 
   //  stops moving for xxx milliseconds.
   //  A good range is 150-350, less than 
   //  150 it's too quick, greater than 350
@@ -31,19 +31,24 @@
 
   function hoverIntent() {
     
-    //  attach the listeners to all the list items
+    //  attach the listeners to all the list items within the nav
     
     li[i].addEventListener("mouseenter", function() {
       
-      //  reference "this" for use later within the stillMouse function
+      //  reference 'this' for use later within the stillMouse function
       
       var b = this;
       
       //  add the class 'hi-hover' when mouse enters the list item
       
-      this.className += " hi-hover";
+      b.className += " hi-hover";
         stillMouse(function() {
-          if(/*b.className.match(/(?:^|\s)hi-hover(?!\S)/) && */!b.className.match(/(?:^|\s)hi-intent(?!\S)/)){
+          
+          //  only add the class 'hi-intent' if the
+          //  mouse is hovering over the element after 250 milliseconds 
+          //  and the class isn't already attached
+          
+          if(b.className.match(/(?:^|\s)hi-hover(?!\S)/) && !b.className.match(/(?:^|\s)hi-intent(?!\S)/)){
             b.className += " hi-intent";
           }
         });
@@ -51,15 +56,26 @@
 
     li[i].addEventListener("mouseleave", function() {
       var b = this;
-      this.className = this.className.replace(/(?:^|\s)hi-hover(?!\S)/g, "");
+      
+      //  removes the class 'hi-hover' when mouse leaves the list item
+      
+      b.className = b.className.replace(/(?:^|\s)hi-hover(?!\S)/g, "");
         stillMouse(function() {
+          
+          //  only remove the class 'hi-intent' if the
+          //  element has the class attached, and if the
+          //  mouse is not hovering over the element after 250 milliseconds 
+          
           if(b.className.match(/(?:^|\s)hi-intent(?!\S)/) && !b.className.match(/(?:^|\s)hi-hover(?!\S)/)){
             b.className = b.className.replace(/(?:^|\s)hi-intent(?!\S)/g, "");
           }
         }); 
     });
-    
   }
+  
+  //  evaluate all list items in the DOM
+  //  if a list item is a child of the nav
+  //  then call the hoverIntent function
   
   for (var i = 0; i < li.length; ++i) {
     var navli = childOf(li[i], nav);
